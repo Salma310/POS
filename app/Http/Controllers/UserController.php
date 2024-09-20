96418de37b3a9086562d0540a2672a3ca0233bba
Salma310 <nasywatwentytwo@gmail.com>
2024-09-20 09:55:45 +0700

    Not Found Exceptions : findOrFail, firstOrFail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -16,18 +16,24 @@ class UserController extends Controller
     public function index(){
 
     //JOBSHEET 4
+    //Praktikum 2.2 JS4 : Not Found Exceptions
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
+        // $user = UserModel::findOrFail(1);
+        return view('user', ['data' => $user]);
+
+        
 
     
     
-    //Praktikum 2.1 JS4
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+    //Praktikum 2.1 JS4 :  Retrieving Single Models
+        // $user = UserModel::findOr(20, ['username', 'nama'], function(){
+        //     abort(404);
+        // });
         // $user= UserModel::where('level_id', 1)->first();
         // $user= UserModel::find(1);
 
         // $user= UserModel::firstWhere('level_id', 1);
-        return view('user', ['data' => $user]);
+        // return view('user', ['data' => $user]);
 
         //Praktikum 1 JS4
         // $data = [
