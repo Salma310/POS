94be5104e92035745815546dcc244475d36a61bf
Salma310 <nasywatwentytwo@gmail.com>
2024-09-20 09:48:37 +0700

    Retrieving Single Models  : find, first, firtsWhere

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -17,25 +17,38 @@ class UserController extends Controller
 
     //JOBSHEET 4
 
+    
+    
+    //Praktikum 2.1 JS4
+        $user = UserModel::findOr(20, ['username', 'nama'], function(){
+            abort(404);
+        });
+        // $user= UserModel::where('level_id', 1)->first();
+        // $user= UserModel::find(1);
+
+        // $user= UserModel::firstWhere('level_id', 1);
+        return view('user', ['data' => $user]);
+
+        //Praktikum 1 JS4
         // $data = [
         //     'level_id' => 2,
         //     'username' => 'manager_dua',
         //     'nama' => 'manager',
         //     'password' => Hash::make('12345')
         // ];
-        $data = [
-            'level_id' => 2,
-            'username' => 'manager_tiga',
-            'nama' => 'Manager 3',
-            'password' => Hash::make('12345')
-        ];
-        UserModel::create($data);
+        // $data = [
+        //     'level_id' => 2,
+        //     'username' => 'manager_tiga',
+        //     'nama' => 'Manager 3',
+        //     'password' => Hash::make('12345')
+        // ];
+        // UserModel::create($data);
 
         
     //JOBSHEET 3
         //coba akses model UserModel
-        $user = UserModel::all(); //Ambil semua data dari tabel m_user
-        return view('user', ['data' => $user]);
+        // $user = UserModel::all(); //Ambil semua data dari tabel m_user
+        // return view('user', ['data' => $user]);
         
        // tambah data user dengan Eloquent Model
         // $data = [
--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -56,7 +56,7 @@
             <br>
             <a href="{{ route('home')}}">Kembali ke Home </a>
         </div> --}}
-        <h1>Data User</h1>
+        <h1>Data User</h1>  
         <table border="1" cellpadding="2", cellspacing="0">
             <tr>
                 <th>ID</th>
@@ -64,14 +64,22 @@
                 <th>Nama</th>
                 <th>ID Level Pengguna</th>
             </tr>
-            @foreach ($data as $d )
+            {{-- Praktikum 4 --}}
+            <tr>
+                <td>{{ $data->user_id }}</td>
+                <td>{{ $data->username }}</td>
+                <td>{{ $data->nama }}</td>
+                <td>{{ $data->level_id }}</td>
+            </tr>  
+            {{-- Praktikum 3 --}}
+            {{-- @foreach ($data as $d )
             <tr>
                 <td>{{ $d->user_id }}</td>
                 <td>{{ $d->username }}</td>
                 <td>{{ $d->nama }}</td>
                 <td>{{ $d->level_id }}</td>
             </tr>            
-            @endforeach
+            @endforeach --}}
         </table>
     </body>
 </html>
