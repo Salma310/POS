2bb146815baef463cfbf2e686478594837fe1dac
Salma310 <nasywatwentytwo@gmail.com>
2024-10-03 01:17:50 +0700

    Modal Ajax Add Data : User

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,10 +6,12 @@ namespace App\Http\Controllers;
 
 use App\Models\LevelModel;
 use Yajra\DataTables\Facades\DataTables;
+use Illuminate\Support\Facades\Validator;
 use App\Models\User;
 use App\Models\UserModel;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Monolog\Level;
 
 class UserController extends Controller
 {
@@ -92,6 +94,44 @@ class UserController extends Controller
 
         return redirect('/user')->with('success', 'Data user berhasil disimpan');
     }
+
+    public function create_ajax(){
+        $level = LevelModel::select('level_id', 'level_nama')->get();
+
+        return view('user.create_ajax')
+                    ->with('level', $level);
+    }
+
+    public function store_ajax(Request $request){
+        //cek apsakah request berupa ajax
+        if($request->ajax() || $request->wantsJson()){
+            $rules = [
+                'level_id' => 'required|integer', 
+                'username' => 'required|string|min:3|unique:m_user,username',
+                'nama' => 'required|string|max:100',
+                'password'=> 'required|min:6'
+            ];
+
+            // use Illuminate\Support\Facades\Validator;
+            $validator = Validator::make($request->all(), $rules);
+
+            if($validator->fails()){
+                return response()->json([
+                'status' => false, // response status, false: error/gagal, true: berhasil
+                'message' => 'Validasi Gagal',
+                'msgField' => $validator->errors(), // pesan error validasi
+                ]);
+            }  
+            
+            UserModel::create($request->all());
+            return response()->json([
+                'status' => true,
+                'message' => 'Data user berhasil disimpan'
+            ]);
+        }
+        return redirect('/');
+    }
+    
 
     public function show(string $id){
         $user = UserModel::with('level')->find($id);
