31f774784d7b4840d2f21ac06adff732ba14d528
Salma310 <nasywatwentytwo@gmail.com>
2024-10-04 22:49:59 +0700

    Supplier Page : CRUD with AJAX

--- routes/web.php
+++ routes/web.php
@@ -112,9 +112,15 @@ Route::group(['prefix' => 'supplier'], function(){
     Route::post('/list', [SupplierController::class, 'list']);      //menampilkan data user dalam bentuk json untuk datatables
     Route::get('/create', [SupplierController::class, 'create']);   //Menampilkan halaman form tambah user
     Route::post('/', [SupplierController::class, 'store']);         //Menyimpan data user baru
+    Route::get('/create_ajax', [SupplierController::class, 'create_ajax']); //Menampilkan halaman form tambah user ajax
+    Route::post('/ajax', [SupplierController::class, 'store_ajax']);      //Menyimpan data user baru ajax  
     Route::get('/{id}', [SupplierController::class, 'show']);       //menampilkan detail user
     Route::get('/{id}/edit', [SupplierController::class, 'edit']);  //menampilkan halaman form edit user
     Route::put('/{id}', [SupplierController::class, 'update']);     //menyimpan perubahan data user
+    Route::get('/{id}/edit_ajax', [SupplierController::class, 'edit_ajax']); //Menampilkan halaman form edit user Ajax
+    Route::put('/{id}/update_ajax', [SupplierController::class, 'update_ajax']);  //Menyimpan perubahan data user
+    Route::get('/{id}/delete_ajax', [SupplierController::class, 'confirm_ajax']);  //tampilan form confirm ddelete user Ajax
+    Route::delete('/{id}/delete_ajax', [SupplierController::class, 'delete_ajax']); //untuk hapus data user ajax
     Route::delete('/{id}', [SupplierController::class, 'destroy']); //mengahapus data user
 });
 
